e68219e72119912b871d279f78b6b42a9fd8afcc
Bernardo Nilson <bernardonilson360@gmail.com>
2025-05-26 20:39:30 -0300

    Add problema 2

--- problema_2.py
+++ problema_2.py
@@ -1,47 +1,61 @@
 import random, string, time
 
 """
-## O problema
+## Enunciado 2
 
-Dadas duas strings, s1 e s2, verificar a posição da primeira ocorrência de se s2 em s1, se existir.
+2. O algoritmo de Rabin-Karp utiliza uma função hash para resolver o problema de busca de padrões em string. O algoritmo está dado abaixo.
+ 
+```javascript
+private int search(String pat, String txt) {
+   int M = pat.length();
+   int N = txt.length();
+   long patHash = hash(pat, M);
 
-Assim, se s1 = "ABCDCBDCBDACBDABDCBADF" e s1 = "ADF" o retorno seria 19.
+   for (int i = 0; i <= N - M; i++) {
+      long txtHash = hash(txt.subtring(i, i+M), M);
+      if (patHash == txtHash)
+         return i; // ocorrência? colisão?
+   }
+   return N; // nenhuma ocorrência
+}
+```
 
-## Enunciado 1
+O hash pode ser calculado utilizando o algoritmo de Horner.
+Algoritmo de Horner para calcular o hash de uma string s[0..M-1]:
 
-1. Faça um algortimo que resolva o problema acima.
-   1. teste-o para strings de diversos tamanhos, até strings grandes (ambas as strings >500.000 caracteres). Conte o número de iterações e de instruções.
-   1. qual a complexidade, no pior caso?
+```javascript
+//
+//Notação: o padrão tem M caracteres, o texto tem N caracteres, o alfabeto tem R caracteres  (0 … R−1) 
+//              Q é o módulo para o cálculo do Hash.
+//              Qual o valor de Q?  Escolha Q igual a um primo grande para minimizar a chance de colisões.
+//                       Por exemplo: o maior primo que possa ser representado com um int
 
+private long hash(String s, int M) {
+   long h = 0;
+   for (int j = 0; j < M; j++)
+      h = (h * R + s.charAt(j)) % Q;
+   return h;
+}
+```
 """
-def find_substring(s1, s2):
-    iterations = 0
-    result = -1
+def hash(s,m):
+   h = 0;
+   for i in range(0, m):
+      h = (h * 21 + s[i]) % 7919
+   return h
 
-    offset = 0
-    index_s1 = 0
-    index_s2 = 0
-    while (index_s1 < len(s1)) and (index_s2 < len(s2)):
-        iterations += 1
+def search(pat, txt):
+    m = len(pat)
+    n = len(txt)
+    pat_hash = hash(pat, m)
 
-        if s1[index_s1] == s2[index_s2]:
-            if result == -1:
-                result = index_s1
-            else:
-                offset += 1
-        else:
-            result = -1
-            index_s2 = -1
-            index_s1 -= offset
-            offset = 0
+    for i in range(0, n - m + 1):
+        txt_hash = hash(txt[i: i + m], m)
+        if (pat_hash == txt_hash):
+            return i
+    return n
 
-        index_s1 += 1
-        index_s2 += 1
-
-    print("Iterations:", iterations)
-    return result
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     print("-"*50)
     
     s1 = "ABCDCBDCBDACBDABDCBADF"
@@ -54,7 +68,7 @@ if __name__ == "__main__":
     print(f'Length S2 = {len(s2)}')
 
     start = time.time()
-    result = find_substring(s1, s2)
+    result = search(s1, s2)
     end = time.time()
 
     print(f'Result = {result}')
@@ -72,7 +86,7 @@ if __name__ == "__main__":
     print(f'Length S2 = {len(s2)}')
 
     start = time.time()
-    result = find_substring(s1, s2)
+    result = search(s1, s2)
     end = time.time()
 
     print(f'Result = {result}')
@@ -90,7 +104,7 @@ if __name__ == "__main__":
     print(f'Length S2 = {len(s2)}')
 
     start = time.time()
-    result = find_substring(s1, s2)
+    result = search(s1, s2)
     end = time.time()
 
     print(f'Result = {result}')
@@ -101,7 +115,7 @@ if __name__ == "__main__":
     s1 = ""
     s2 = ""
 
-    for i in range(50000):
+    for i in range(5000):
         s1 += random.choice(string.ascii_letters)
 
     for i in range(5):
@@ -114,11 +128,10 @@ if __name__ == "__main__":
     print(f'Length S2 = {len(s2)}')
 
     start = time.time()
-    result = find_substring(s1, s2)
+    result = search(s1, s2)
     end = time.time()
 
     print(f'Result = {result}')
     print(f'Time = {end - start}')
 
     print("-"*50)
-    
