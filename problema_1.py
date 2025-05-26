cb3b3f8b37c4add67fa3202f4edbeead00c4c624
Bernardo Nilson <bernardonilson360@gmail.com>
2025-05-26 20:14:18 -0300

    Add problem 1

--- problema_1.py
+++ problema_1.py
@@ -1,3 +1,5 @@
+import random, string, time
+
 """
 ## O problema
 
@@ -13,27 +15,111 @@ Assim, se s1 = "ABCDCBDCBDACBDABDCBADF" e s1 = "ADF" o retorno seria 19.
 
 """
 def find_substring(s1, s2):
+    iterations = 0
     result = -1
 
+    offset = 0
     index_s1 = 0
     index_s2 = 0
-    while (index_s1 < len(s1) - 1) and (index_s2 < len(s2) - 1):
+    while (index_s1 < len(s1)) and (index_s2 < len(s2)):
+        iterations += 1
+
         if s1[index_s1] == s2[index_s2]:
             if result == -1:
                 result = index_s1
+            else:
+                offset += 1
         else:
             result = -1
-            index_s2 = 0
+            index_s2 = -1
+            index_s1 -= offset
+            offset = 0
 
         index_s1 += 1
         index_s2 += 1
 
+    print("Iterations:", iterations)
     return result
 
 if __name__ == "__main__":
+    
+    print("-"*50)
+    
     s1 = "ABCDCBDCBDACBDABDCBADF"
     s2 = "ADF"
 
+    print(f'S1 = "{s1}"')
+    print(f'Lenght S1 = {len(s1)}')
+    
+    print(f'S2 = "{s2}"')
+    print(f'Length S2 = {len(s2)}')
+
+    start = time.time()
     result = find_substring(s1, s2)
-    print(f'S1 = "{s2}", S2 "{s1}" : {result}')
+    end = time.time()
+
+    print(f'Result = {result}')
+    print(f'Time = {end - start}')
+
+    print("-"*50)
+    
+    s1 = "ABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFABCDCBDCBDACBDABDCBADFOOOOOOO"
+    s2 = "OOOOOOO"
+
+    print(f'S1 = "{s1}"')
+    print(f'Lenght S1 = {len(s1)}')
+    
+    print(f'S2 = "{s2}"')
+    print(f'Length S2 = {len(s2)}')
+
+    start = time.time()
+    result = find_substring(s1, s2)
+    end = time.time()
+
+    print(f'Result = {result}')
+    print(f'Time = {end - start}')
+
+    print("-"*50)
+
+    s1 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAB"
+    s2 = "AAAB"
+
+    print(f'S1 = "{s1}"')
+    print(f'Lenght S1 = {len(s1)}')
+    
+    print(f'S2 = "{s2}"')
+    print(f'Length S2 = {len(s2)}')
+
+    start = time.time()
+    result = find_substring(s1, s2)
+    end = time.time()
+
+    print(f'Result = {result}')
+    print(f'Time = {end - start}')
+
+    print("-"*50)
+
+    s1 = ""
+    s2 = ""
+
+    for i in range(5000):
+        s1 += random.choice(string.ascii_letters)
+
+    for i in range(5):
+        s2 += random.choice(string.ascii_letters)
+
+    print(f'S1 = "{s1}"')
+    print(f'Lenght S1 = {len(s1)}')
+    
+    print(f'S2 = "{s2}"')
+    print(f'Length S2 = {len(s2)}')
+
+    start = time.time()
+    result = find_substring(s1, s2)
+    end = time.time()
+
+    print(f'Result = {result}')
+    print(f'Time = {end - start}')
+
+    print("-"*50)
     
